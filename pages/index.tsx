20aee5b9accae2ca5156cfbf46a73aec80007309
Juncker Maxime <82824938+Maxime-juncker@users.noreply.github.com>
2023-12-19 17:47:04 +0100

    fix vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,9 +12,7 @@ const green = "#387256";
 export default function Home() {
   return (
       <main className="App">
-          <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
+        
               {/* <Navbar/> */}
         
                 <section className='Hero' id='Hero'>
